c1fcc19b370df4c44fa1455f2fdfeb468bbc5a2e
yixinBC <yixinbc@foxmail.com>
2025-05-03 17:04:41 +0800

    添加 wiki 应用的 URL 配置和视图，更新设置以使用 WikiConfig

--- zlbcwiki/wiki/apps.py
+++ zlbcwiki/wiki/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class WikiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'wiki'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "wiki"
+    urls = "wiki.apps.urls"
--- zlbcwiki/zlbcwiki/settings.py
+++ zlbcwiki/zlbcwiki/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "wiki",
+    "wiki.apps.WikiConfig",
 ]
 
 MIDDLEWARE = [
